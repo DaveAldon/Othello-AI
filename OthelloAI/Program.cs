5eaef3f67988de68b7f3e8b1ed32b741c9d7fde7
David Crawford <crawford@mail.gvsu.edu>
2016-09-01 00:12:04 -0400

    Initialization of project. Program.cs also displays the 8x8 othello game board.

---
+++ OthelloAI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OthelloAI
+{
+	class MainClass
+	{
+		public int[,] GameBoard = new int[8, 8];
+
+		void displayBoard()
+		{
+			for (int i = 0; i < 8; i++)
+			{
+				for (int ii = 0; ii < 8; ii++)
+				{
+					Console.Write(Convert.ToString(GameBoard[i, ii]));
+					if (ii != 0 && ii % 7 == 0) //Simple way of making it look nice in output for now, might be a better way later
+					{
+						Console.Write("\n");
+					}
+				}
+			}
+		}
+
+		public static void Main(string[] args)
+		{
+			MainClass Othello = new MainClass();	
+			Othello.displayBoard();
+		}
+	}
+}
